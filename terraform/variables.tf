6537eff6fc8aea872417e53080649fdd704c8874
irene <irene_romero_95@hotmail.com>
2026-04-23 15:51:06 +0200

    variables for vpc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,21 @@ variable "project_name" {
   type        = string
   default     = "project-1-joao-irene"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets (one per AZ)."
+  type        = list(string)
+  default     = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets (one per AZ)."
+  type        = list(string)
+  default     = ["10.20.11.0/24", "10.20.12.0/24"]
+}
