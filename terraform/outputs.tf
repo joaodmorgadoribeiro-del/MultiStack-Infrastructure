6994dc9f16c6f38c9a5cfacc23f513f727e08b83
Joao <joaodmorgadoribeiro@gmail.com>
2026-04-23 17:23:00 +0100

    feat: changes on outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,11 +10,17 @@ output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
 
+output "frontend_asg_name" {
+  description = "Check this in the console to find the Public IP of the Bastion Host."
+  value       = aws_autoscaling_group.frontend.name
+}
 
 output "backend_private_ip" {
-  value = aws_instance.backend.private_ip
+  description = "Private IP of the Backend. Needed for Ansible inventory."
+  value       = aws_instance.backend.private_ip
 }
 
 output "database_private_ip" {
-  value = aws_instance.database.private_ip
+  description = "Private IP of the Database. Needed for Ansible inventory."
+  value       = aws_instance.database.private_ip
 }
