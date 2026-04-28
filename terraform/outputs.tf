1b1fe13c47fe729860993b9356bae523a7a36261
Joao <joaodmorgadoribeiro@gmail.com>
2026-04-28 15:15:21 +0100

    feat: Infrastructure Updated

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,32 +1,3 @@
-# output "vpc_id" {
-#   value = module.vpc.vpc_id
-# }
-
-# output "public_subnet_id" {
-#   value = module.vpc.public_subnet_ids[0]
-# }
-
-# output "private_subnet_backend_id" {
-#   value = module.vpc.private_subnet_ids[0]
-# }
-
-# output "private_subnet_db_id" {
-#   value = module.vpc.private_subnet_ids[1]
-# }
-
-# output "frontend_public_ip" {
-#   value = module.frontend.instance_public_ip
-# }
-
-# output "backend_private_ip" {
-#   value = module.backend.instance_private_ip
-# }
-
-# output "database_private_ip" {
-#   value = module.database.instance_private_ip
-# }
-
-# ─── VPC ───────────────────────────────────────────────────────────────────────
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
@@ -85,10 +56,3 @@ output "result_app_url" {
   value       = "http://${aws_lb.main.dns_name}/result"
 }
 
-# output "vote_app_url" {
-#   value = "http://${module.frontend.instance_public_ip}:5000"
-# }
-
-# output "result_app_url" {
-#   value = "http://${module.frontend.instance_public_ip}:4000"
-# }
